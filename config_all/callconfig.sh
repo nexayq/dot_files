f4adde72c5dcc10f199b7fc8af92e7cb4e0c59f8
nexayq <nexayq@users.noreply.github.com>
2016-09-22 06:31:38 +0200

    Config_all - Initial script callconfig.sh -> tmux, vim, bash and git

---
+++ config_all/callconfig.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Make directories
+cd $HOME
+CONFIG_DIR=config_nk
+mkdir -p $CONFIG_DIR
+mkdir -p .config/nvim
+
+# Clone repos
+cd $HOME/$CONFIG_DIR
+# NK
+git clone https://bitbucket.org/nexayq/config
+git clone https://github.com/nexayq/dot_files
+# Others
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+
+# Link files
+cd $HOME
+ln -s $CONFIG_DIR/dot_files/tmux/dot_tmux.conf .tmux.conf
+ln -s $CONFIG_DIR/dot_files/vim/dot_vimrc .vimrc
+ln -s $CONFIG_DIR/dot_files/git/dot_gitconfig .gitconfig
+ln -s $CONFIG_DIR/dot_files/git/dot_git-prompt-colors.sh .git-prompt-colors.sh
+ln -s $CONFIG_DIR/config/bash/callback.sh callback.sh
+ln -s .vimrc .config/nvim/init.vim
+
+# Tweaks
+cd $HOME
+touch .nk_history
+
+# Add source ~/callback.sh to .bashrc
