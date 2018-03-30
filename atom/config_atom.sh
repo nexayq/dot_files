1f0f559e3ee172dc7d3e527535fa96e9feb4eaf5
nexayq <nexayq@users.noreply.github.com>
2018-03-30 17:14:40 +0200

    Atom - Added config_atom.sh to link only Atom config files

---
+++ atom/config_atom.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Make directories
+    cd $HOME
+    CONFIG_DIR=config_nk
+    mkdir -p .atom
+
+# Link files
+    # Atom config
+    cd $HOME/.atom
+    ln -s $CONFIG_DIR/dot_files/atom/config.cson config.cson
+    ln -s $CONFIG_DIR/dot_files/atom/packages.cson packages.cson
+    ln -s $CONFIG_DIR/dot_files/atom/keymap.cson keymap.cson
+    cd $HOME
