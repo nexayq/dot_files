82ac538a6f0c43f05a1a01f166f90348751395b1
nexayq <nexayq@users.noreply.gitlab.com>
2020-11-28 08:58:32 +0100

    scripts - Restart NetworkManager and nm-applet

--- linux/scripts/restart_nm-applet.sh
+++ linux/scripts/restart_nm-applet.sh
@@ -1,4 +1,10 @@
 #/bin/bash
 
+# Restart Network Manager
+sudo systemctl stop NetworkManager
+sudo systemctl start NetworkManager
+
+sleep 5
+
 # restart nm-applet
 killall nm-applet; setsid nm-applet &
